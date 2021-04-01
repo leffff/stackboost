3c73eb7e91398de4d016f267dbf88cf1c7dc0d3d
leffff <levnovitskiy@gmail.com>
2021-04-01 18:10:59 +1000

    Stacked Regression and Classification trees added

---
+++ stackboost/__init__.py
@@ -0,0 +1,6 @@
+# from stackboost.tree import StackedTreeClassifier
+# from stackboost.boosting import AdaptiveBoostingClassifier
+
+__author__ = 'Lev Novitskiy > levnovitskiy@gmail.com > https://github.com/leffff/ > https://www.kaggle.com/levnovitskiy'
+__license__ = 'Apache'
+__version__ = '2.0'
