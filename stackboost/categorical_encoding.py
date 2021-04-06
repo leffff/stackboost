Gradient boosting regressor fixed 2 TreeRegressor types added to stackboost.models.tree.py Categorical encoder added

stackboost/categorical_encoding.py
@@ -16,6 +16,7 @@ class StackedEncoder:
     """
     Categorical feature incoding model
     """
+
     def __init__(self, strategy: str = "mean", regression: bool = True):
         self.strategy = strategy
         if self.strategy != None:
@@ -25,7 +26,8 @@ class StackedEncoder:
 
             self.strategy = self.strategy.lower()
             if self.strategy not in list(self.strategies.keys()):
-                raise ValueError("Given strategy type", self.strategy, "allowed," ', '.join(list(self.strategies.keys())))
+                raise ValueError("Given strategy type", self.strategy,
+                                 "allowed," ', '.join(list(self.strategies.keys())))
         self.regression = regression
         self.fitted = False
 
@@ -41,6 +43,8 @@ class StackedEncoder:
             self.cat_features = np.array([i for i in range(self.n_features)])
 
         self.__target(X, y)
+
+        self.fitted = True
 
     def __target(self, X: np.ndarray, y: np.ndarray) -> None:
         if self.regression:
@@ -67,6 +71,9 @@ class StackedEncoder:
                 self.mappers = np.append(self.mappers, counts)
 
     def transform(self, X: np.ndarray) -> np.ndarray:
+        if not self.fitted:
+            raise NotFittedError()
+
         X_array = to_array(X)
         n_features_test = X_array.shape[1]
         if self.n_features != n_features_test:

stackboost/models/gradient_boosting.py
@@ -8,17 +8,23 @@ from copy import copy
 
 
 class StackedGradientBoostingRegressor:
-    def __init__(self, estimator=None, n_estimators=50, learning_rate=0.01, loss_function=SquareLoss()):
+    def __init__(self, estimator=None, n_estimators=50, learning_rate=0.01, loss_function=SquareLoss(), verbose: bool=True):
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
         self.loss_function = loss_function
         self.model = estimator
+        self.verbose=verbose
         self.models = None
         self.fitted = False
 
     def fit(self, X, y, eval_set=None, cat_features=None):
         X, y = to_array(X), to_array(y)
-        X_test, y_test = eval_set
+
+        test_pres = False
+        if eval_set != None:
+            test_pres = True
+            X_test, y_test = eval_set
+
         self.initial_preds = np.array([y.mean() for i in range(len(y))])
         residuals = self.loss_function.gradient(y, self.initial_preds)
 
@@ -29,7 +35,6 @@ class StackedGradientBoostingRegressor:
         pseudo_residuals = [residuals]
 
         for i in range(self.n_estimators):
-            print(i)
             model = copy(self.model)
             model.fit(X, pseudo_residuals[-1], cat_features=cat_features)
             self.models.append(model)
@@ -37,8 +42,19 @@ class StackedGradientBoostingRegressor:
             preds = model.predict(X) * self.learning_rate + predictions[-1]
             predictions.append(preds)
 
-            self.train_errors.append(self.loss_function.loss(y, preds))
-            self.test_errors.append(self.loss_function.loss(y_test, self.__inner_predict(X_test)))
+            train_error = self.loss_function.loss(y, preds)
+            self.train_errors.append(train_error)
+
+            if test_pres:
+                test_terror = self.loss_function.loss(y_test, self.__inner_predict(X_test))
+                self.test_errors.append(test_terror)
+
+            if self.verbose:
+                train_print = f"Estimator: {i} Train: {train_error}"
+                test_print = ""
+                if test_pres:
+                    test_print = "Test: {test_terror}"
+                print(train_print + test_print)
 
             residuals -= self.loss_function.gradient(y, preds)
             pseudo_residuals.append(residuals)
@@ -53,6 +69,9 @@ class StackedGradientBoostingRegressor:
         return y_pred
 
     def plot(self):
+        if not self.fitted:
+            raise NotFittedError()
+
         iterations = [i for i in range(self.n_estimators)]
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=iterations, y=self.train_errors,