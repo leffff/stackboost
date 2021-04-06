Gradient boosting regressor fixed 2 TreeRegressor types added to stackboost.models.tree.py Categorical encoder added

stackboost/models/gradient_boosting.py
@@ -1,28 +1,30 @@
 from stackboost.utils.data_manipulation import to_array
-from stackboost.models.split_test import StackedTreeRegressor
+from stackboost.models.tree_test import StackedTreeRegressor
 from stackboost.loss_functions import MSE, SquareLoss
+from stackboost.utils.errors import NotFittedError
+import plotly.graph_objects as go
 import numpy as np
 from copy import copy
 
 
 class StackedGradientBoostingRegressor:
-    def __init__(self, leaf_estimaor=None, n_estimators=50, max_depth=15, learning_rate=0.1, loss_function=SquareLoss()):
-        self.leaf_estimator = leaf_estimaor
+    def __init__(self, estimator=None, n_estimators=50, learning_rate=0.01, loss_function=SquareLoss()):
         self.n_estimators = n_estimators
-        self.max_depth = max_depth
         self.learning_rate = learning_rate
         self.loss_function = loss_function
-        self.model = StackedTreeRegressor(leaf_estimator=self.leaf_estimator, max_depth=self.max_depth)
+        self.model = estimator
         self.models = None
         self.fitted = False
 
-    def fit(self, X, y, cat_features=None):
+    def fit(self, X, y, eval_set=None, cat_features=None):
         X, y = to_array(X), to_array(y)
-
+        X_test, y_test = eval_set
         self.initial_preds = np.array([y.mean() for i in range(len(y))])
         residuals = self.loss_function.gradient(y, self.initial_preds)
 
         self.models = []
+        self.train_errors = []
+        self.test_errors = []
         predictions = [self.initial_preds]
         pseudo_residuals = [residuals]
 
@@ -35,16 +37,36 @@ class StackedGradientBoostingRegressor:
             preds = model.predict(X) * self.learning_rate + predictions[-1]
             predictions.append(preds)
 
+            self.train_errors.append(self.loss_function.loss(y, preds))
+            self.test_errors.append(self.loss_function.loss(y_test, self.__inner_predict(X_test)))
+
             residuals -= self.loss_function.gradient(y, preds)
             pseudo_residuals.append(residuals)
 
+        self.fitted = True
 
-    def predict(self, X):
+    def __inner_predict(self, X):
         X = to_array(X)
         y_pred = np.array([self.initial_preds[0] for i in range(len(X))])
-        for i in range(self.n_estimators):
+        for i in range(len(self.models)):
             y_pred += self.models[i].predict(X) * self.learning_rate
-
         return y_pred
 
+    def plot(self):
+        iterations = [i for i in range(self.n_estimators)]
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=iterations, y=self.train_errors,
+                                 mode='lines',
+                                 name='train'))
+        fig.add_trace(go.Scatter(x=iterations, y=self.test_errors,
+                                 mode='lines',
+                                 name='test'))
 
+        fig.show()
+
+    def predict(self, X):
+        if not self.fitted:
+            raise NotFittedError()
+
+        X = to_array(X)
+        return self.__inner_predict(X)

stackboost/models/tree.py
@@ -2,12 +2,17 @@ from __future__ import division, print_function
 import numpy as np
 
 from stackboost.utils.data_manipulation import divide_on_feature, train_test_split, standardize, to_array
-from stackboost.utils.data_operation import calculate_entropy, accuracy_score, calculate_variance, calculate_covariance
+from stackboost.utils.data_operation import calculate_entropy, accuracy_score, calculate_variance, \
+    calculate_correlation, calculate_correlation_matrix, calculate_covariance, calculate_dispersion, mean_squared_error, \
+    similarity_score
+from stackboost.categorical_encoding import StackedEncoder
 import torch
 import pandas as pd
 from sklearn.linear_model import LogisticRegression, LinearRegression
 import warnings
 import copy
+from numba import jit, int32, float32
+from numba.experimental import jitclass
 
 warnings.filterwarnings("ignore")
 SUPPORTED_ARRAYS = [np.ndarray, torch.tensor, pd.Series, pd.DataFrame, list]
@@ -30,7 +35,7 @@ class DecisionNode():
         Next decision node for samples where features value did not meet the threshold.
     """
 
-    def __init__(self, feature_i: int=None, threshold: float=None, value=None, true_branch=None, false_branch=None):
+    def __init__(self, feature_i=None, threshold=None, value=None, true_branch=None, false_branch=None):
         self.feature_i = feature_i  # Index for the feature that is tested
         self.threshold = threshold  # Threshold value for feature
         self.value = value  # Value if the node is a leaf in the tree
@@ -38,7 +43,6 @@ class DecisionNode():
         self.false_branch = false_branch  # 'Right' subtree
 
 
-# Super class of RegressionTree and ClassificationTree
 class DecisionTree(object):
     """Super class of RegressionTree and ClassificationTree.
     Parameters:
@@ -53,7 +57,8 @@ class DecisionTree(object):
         Loss function that is used for Gradient Boosting models to calculate impurity.
     """
 
-    def __init__(self, leaf_estimator = None, min_samples_split=2, min_impurity=1e-7, max_depth=float("inf"), loss=None):
+    def __init__(self, leaf_estimator=None, min_samples_split: int = 2, min_impurity: float = 1e-7,
+                 max_depth: int = float("inf"), l2_leaf_reg: float = 0.0, loss_function=None):
         self.leaf_estimator = leaf_estimator
         self.root = None  # Root node in dec. tree
         # Minimum n of samples to justify split
@@ -62,25 +67,33 @@ class DecisionTree(object):
         self.min_impurity = min_impurity
         # The maximum depth to grow the tree to
         self.max_depth = max_depth
+        self.l2_leaf_reg = l2_leaf_reg
         # Function to calculate impurity (classif.=>info gain, regr=>variance reduct.)
         self._impurity_calculation = None
         # Function to determine prediction of y at leaf
-        self._leaf_value_calculation = None
+        self.leaf_training = None
         # If y is one-hot encoded (multi-dim) or not (one-dim)
         self.one_dim = None
         # If Gradient Boost
-        self.loss = loss
+        self.cat_features = None
+        self.sample_weight = None
+        self.loss_function = loss_function
+        self.encoder = StackedEncoder()
 
-    def fit(self, X, y, sample_weight=None, loss=None):
+    def fit(self, X, y, sample_weight=None, cat_features=None):
         """ Build decision tree """
         X, y = to_array(X), to_array(y)
+        self.cat_features = cat_features
+        if self.cat_features in SUPPORTED_ARRAYS:
+            X = self.encoder.fit_transform(X, y, cat_features=self.cat_features)
+
+        self.sample_weight = sample_weight
         self.n_features = X.shape[1]
         if type(sample_weight) not in SUPPORTED_ARRAYS:
             self.sample_weight = 1 / np.array([1 for i in range(self.n_features)])
 
         self.one_dim = len(np.shape(y)) == 1
         self.root = self._build_tree(X, y)
-        self.loss = None
 
     def _build_tree(self, X, y, current_depth=0):
         """ Recursive method which builds out the decision tree and splits X and respective y
@@ -98,7 +111,7 @@ class DecisionTree(object):
 
         n_samples, n_features = np.shape(X)
 
-        if n_samples >= self.min_samples_split and current_depth <= self.max_depth - 1:
+        if n_samples >= self.min_samples_split and current_depth <= self.max_depth:
             # Calculate the impurity for each feature
             for feature_i in range(n_features):
                 # All values of feature_i
@@ -114,11 +127,9 @@ class DecisionTree(object):
 
                     if len(Xy1) > 0 and len(Xy2) > 0:
                         # Select the y-values of the two sets
-                        y1 = Xy1[:, n_features:]
-                        y2 = Xy2[:, n_features:]
 
                         # Calculate impurity
-                        impurity = self._impurity_calculation(y, y1, y2) * self.sample_weight[feature_i]
+                        impurity = self._impurity_calculation(y, Xy1, Xy2) * self.sample_weight[feature_i]
 
                         # If this threshold resulted in a higher information gain than previously
                         # recorded save the threshold value and the feature
@@ -175,7 +186,11 @@ class DecisionTree(object):
 
     def predict(self, X):
         """ Classify samples one by one and return the set of labels """
-        y_pred = [self.predict_value(sample) for sample in X]
+        if self.cat_features in SUPPORTED_ARRAYS:
+            print("yes")
+            X = self.encoder.transform(X)
+
+        y_pred = np.array([self.predict_value(sample) for sample in X])
         return y_pred
 
     def print_tree(self, tree=None, indent=" "):
@@ -197,18 +212,26 @@ class DecisionTree(object):
             self.print_tree(tree.false_branch, indent + indent)
 
 
-class StackedTreeRegressor(DecisionTree):
-    def _calculate_variance_reduction(self, y, y1, y2):
-        var_tot = calculate_variance(y)
-        var_1 = calculate_variance(y1)
-        var_2 = calculate_variance(y2)
+class DispersionTreeRegressor(DecisionTree):
+    def _calculate_variance_reduction(self, y, Xy1, Xy2):
+        y1, X1 = Xy1[:, self.n_features:], Xy1[:, :self.n_features]
+        y2, X2 = Xy2[:, self.n_features:], Xy2[:, :self.n_features]
+
+        tot_disp = calculate_dispersion(y)
+        disp_1 = calculate_dispersion(y1)
+        disp_2 = calculate_dispersion(y2)
+
         frac_1 = len(y1) / len(y)
         frac_2 = len(y2) / len(y)
 
-        # Calculate the variance reduction
-        variance_reduction = var_tot - (frac_1 * var_1 + frac_2 * var_2)
+        total = tot_disp
+        left = frac_1 * disp_1
+        right = frac_2 * disp_2
 
-        return sum(variance_reduction)
+        variance_reduction = total - (left + right)
+
+        # loss_reduction = self.loss_function(y1, mean_1) + self.loss_function(y2, mean_2)
+        return variance_reduction
 
     def regression_leaf_training(self, X, y):
         n_unique = np.unique(y)
@@ -220,15 +243,80 @@ class StackedTreeRegressor(DecisionTree):
         model.fit(X, y)
         return model
 
-    # ADD: categorical_encoding
-    def fit(self, X, y, sample_weight=None):
+    def fit(self, X, y, sample_weight=None, cat_features=None):
         self._impurity_calculation = self._calculate_variance_reduction
         self.leaf_training = self.regression_leaf_training
-        super(StackedTreeRegressor, self).fit(X, y, sample_weight)
+        super(DispersionTreeRegressor, self).fit(X, y, sample_weight, cat_features)
+
+
+class SimilarityTreeRegressor(DecisionTree):
+    def _calculate_variance_reduction(self, y, Xy1, Xy2):
+        y1, X1 = Xy1[:, self.n_features:], Xy1[:, :self.n_features]
+        y2, X2 = Xy2[:, self.n_features:], Xy2[:, :self.n_features]
+
+        gain_1 = similarity_score(y1, self.l2_leaf_reg)
+        gain_2 = similarity_score(y2, self.l2_leaf_reg)
+        gain_tot = similarity_score(y, self.l2_leaf_reg)
+
+        variance_reduction = gain_1 + gain_2 - gain_tot
+
+        return variance_reduction
+
+    def regression_leaf_training(self, X, y):
+        value = np.mean(y, axis=0)
+        return value if len(value) > 1 else value[0]
+
+    def fit(self, X, y, sample_weight=None, cat_features=None):
+        self._impurity_calculation = self._calculate_variance_reduction
+        self.leaf_training = self.regression_leaf_training
+        super(SimilarityTreeRegressor, self).fit(X, y, sample_weight, cat_features)
+
+
+class ErrorTreeRegressor(DecisionTree):
+    def _calculate_variance_reduction(self, y, Xy1, Xy2):
+        y1, X1 = Xy1[:, self.n_features:], Xy1[:, :self.n_features]
+        y2, X2 = Xy2[:, self.n_features:], Xy2[:, :self.n_features]
+
+        tot_disp = calculate_dispersion(y)
+        disp_1 = calculate_dispersion(y1)
+        disp_2 = calculate_dispersion(y2)
+
+        mean_1 = np.zeros(len(y1))
+        mean_1.fill(y1.mean())
+        mean_2 = np.zeros(len(y2))
+        mean_2.fill(y2.mean())
+        mean_tot = np.zeros(len(y))
+        mean_tot.fill(y.mean())
+
+        total = tot_disp * self.loss_function.loss(y, mean_tot) / (len(y) + self.l2_leaf_reg)
+        left = disp_1 * self.loss_function.loss(y1, mean_1) / (len(y1) + self.l2_leaf_reg)
+        right = disp_2 * self.loss_function.loss(y2, mean_2) / (len(y2) + self.l2_leaf_reg)
+
+        variance_reduction = total - (left + right)
+
+        # loss_reduction = self.loss_function(y1, mean_1) + self.loss_function(y2, mean_2)
+        return variance_reduction
+
+    def regression_leaf_training(self, X, y):
+        n_unique = np.unique(y)
+        if self.leaf_estimator == None or len(n_unique) == 1:
+            value = np.mean(y, axis=0)
+            return value if len(value) > 1 else value[0]
+
+        model = copy.copy(self.leaf_estimator)
+        model.fit(X, y)
+        return model
+
+    def fit(self, X, y, sample_weight=None, cat_features=None):
+        self._impurity_calculation = self._calculate_variance_reduction
+        self.leaf_training = self.regression_leaf_training
+        super(ErrorTreeRegressor, self).fit(X, y, sample_weight, cat_features)
 
 
 class StackedTreeClassifier(DecisionTree):
-    def _calculate_information_gain(self, y, y1, y2):
+    def _calculate_information_gain(self, y, Xy1, Xy2):
+        y1 = Xy1[:, self.n_features:]
+        y2 = Xy2[:, self.n_features:]
         # Calculate information gain
         p = len(y1) / len(y)
         entropy = calculate_entropy(y)
@@ -253,8 +341,7 @@ class StackedTreeClassifier(DecisionTree):
         model.fit(X, y)
         return model
 
-    # ADD: categorical_encoding
-    def fit(self, X, y, sample_weight=None):
+    def fit(self, X, y, sample_weight=None, cat_features=None):
         self._impurity_calculation = self._calculate_information_gain
         self.leaf_training = self.classification_leaf_training
-        super(StackedTreeClassifier, self).fit(X, y, sample_weight)
+        super(StackedTreeClassifier, self).fit(X, y, sample_weight, cat_features)